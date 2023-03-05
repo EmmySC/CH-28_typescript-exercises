827f24b2f77fd22e11515b08570ec1b5e766f78d
EmmyC <esc@Emilys-MBP-2.attlocal.net>
2023-03-05 13:19:10 -0600

    changes in Rocket.ts file

--- studio/Rocket.ts
+++ studio/Rocket.ts
@@ -2,13 +2,13 @@ import { Payload } from './Payload';
 import { Astronaut } from './Astronaut'; 
 import { Cargo } from './Cargo';
 
-export class Rocket implements Payload {  //implements Payload
+export class Rocket {  //don't need to add --> implements Payload?
     // properties and methods
     name: string;
     totalCapacityKg: number;
     cargoItems: Cargo [] = [];  //NOT string [] 
     astronauts: Astronaut [] = []; //NOT string []
-    massKg: number;  //from Payload
+    //massKg: number;  //need from Payload?
 
     constructor(name: string, totalCapacityKg: number) {
         this.name = name;
@@ -16,15 +16,15 @@ export class Rocket implements Payload {  //implements Payload
     }
 
     sumMass(items: Payload []): number { //(items: Payload[])
-        let sum: number = 0; //
-        for (let i = 0; i < items.length; i++) { //
-            sum += items[i].massKg; //
+        let sum: number = 0; // accumulator pattern  (can also use --> let sum = 0;)
+        for (let i = 0; i < items.length; i++) { //For loop for array
+            sum += items[i].massKg; // += will keep adding each element to the sum
         }
         return sum; 
     }
 
     currentMassKg(): number {
-        return this.sumMass(this.astronauts) + this.sumMass(this.cargoItems); 
+        return this.sumMass(this.astronauts) + this.sumMass(this.cargoItems); // will loop through the arrays and take the masses to add
     }
 
     canAdd(item: Payload): boolean {
@@ -32,10 +32,10 @@ export class Rocket implements Payload {  //implements Payload
     }
 
     addCargo(cargo: Cargo): boolean {
-        if (this.canAdd(cargo)) {
+        if (this.canAdd(cargo)) {  //needs conditional
             this.cargoItems.push(cargo); //push into cargoItems array
             return true;
-        } else {
+        } else {   //don't need to use else, just the return false
             return false;
         }
     }
@@ -44,7 +44,7 @@ export class Rocket implements Payload {  //implements Payload
         if (this.canAdd(astronaut)) {
             this.astronauts.push(astronaut); //push into astronauts array
             return true;
-        } else {
+        } else {   //don't need to use else, just the return false
             return false;
         }
     }
